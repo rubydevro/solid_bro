ff54f0844c1ef3422fc291ad64239e17b96ae6ab
Emanuel Comsa <cremzinc@gmail.com>
2026-02-13 10:15:37 +0100

    Pundit

--- app/controllers/solid_bro/application_controller.rb
+++ app/controllers/solid_bro/application_controller.rb
@@ -6,6 +6,24 @@ module SolidBro
     elsif defined?(Pagy::Method)
       include Pagy::Method
     end
+
+    # Optional Pundit integration (only if the host app uses Pundit)
+    if defined?(Pundit::Authorization)
+      include Pundit::Authorization
+
+      before_action :authorize_solid_bro!
+
+      private
+
+      # Expects a SolidBroPolicy in the host app:
+      #   class SolidBroPolicy < Struct.new(:user, :solid_bro)
+      #     def access?; user&.admin?; end
+      #   end
+      def authorize_solid_bro!
+        authorize :solid_bro, :access?
+      end
+    end
+
     protect_from_forgery with: :exception
   end
 end
